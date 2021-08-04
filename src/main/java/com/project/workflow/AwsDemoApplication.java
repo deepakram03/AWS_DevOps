86b38d3d8e0f33662ace6f94bff7804f9f1f1dc1
deepak <deepak@172.22.210.13>
2021-08-04 14:08:48 +0300

    commit

--- src/main/java/com/project/workflow/AwsDemoApplication.java
+++ src/main/java/com/project/workflow/AwsDemoApplication.java
@@ -1,5 +1,8 @@
 package com.project.workflow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +19,15 @@ public class AwsDemoApplication {
 	@GetMapping("/demo")  
 	public String demo()
 	{     
-	return "Hi Buddy, Ur automation is success";  
+	return "Hi Buddy, Ur automation is success. Goto the next level";  
 	}  
 
+	//Below code is for handling put argument request
+	public Map<String,String> putAWSAliens(String value) {
+	
+		Map<String,String> awsMap = new HashMap<>();
+		return awsMap;
+	}
+	
+	
 }
